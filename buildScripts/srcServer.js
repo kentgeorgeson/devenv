e673af02efdd3d8d85f1ff62ac6b796233f9f293
Kent Georgeson <kent@Kents-MacBook-Pro.local>
2016-11-18 12:07:20 -0600

    mock data schema

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,15 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+    // Hard coding for simplicity
+    res.json([
+        {"id": 1, "firstname":"Bob", "lastName":"Smith", "email": "bob@gmail.com"},
+        {"id": 2, "firstname":"Tammy", "lastName":"Norton", "email": "tammy@gmail.com"},
+        {"id": 3, "firstname":"Tina", "lastName":"Lee", "email": "tina@gmail.com"}
+    ]);
+});
+
 app.listen(port, function(err) {
     if (err) {
         console.log(err);
